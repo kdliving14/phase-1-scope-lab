c85e52d8a93ef394533bc46fe60159c4237436b5
Karter <kdlmuzixnerd14@gmail.com>
2022-06-29 08:30:11 -0700

    commit tree 8eb2a7022f38fca33d372d914e943d1e15c8257e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+const leastFavoriteCustomer = "bleh";
+
+var bestCustomer;
+
+function upperCaseCustomerName()
+{
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer()
+{
+    return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer()
+{
+    return bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer()
+{
+    return leastFavoriteCustomer = "bob";
+}
